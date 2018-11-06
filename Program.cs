34e936cd03dd004999c4119bfd9c34778331d8eb
Idílio Casimiro <idilio.casimiro@mediplus.co.ao>
2018-11-06 14:51:08 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace .NETCoreIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
